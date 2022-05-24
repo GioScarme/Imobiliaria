41bfb24d813c17b059260653bbb1b9c483be0067
GioScarme <scarmegio@gmail.com>
2022-05-24 13:55:49 -0300

    PhaseListener da Autenticação

--- ImobiliariaTG/src/main/java/br/com/imobiliaria/util/AutenticacaoListener.java
+++ ImobiliariaTG/src/main/java/br/com/imobiliaria/util/AutenticacaoListener.java
@@ -4,12 +4,34 @@ import javax.faces.event.PhaseEvent;
 import javax.faces.event.PhaseId;
 import javax.faces.event.PhaseListener;
 
+import org.omnifaces.util.Faces;
+
+import br.com.imobiliaria.bean.LoginBean;
+import br.com.imobiliaria.domain.Usuario;
+
 @SuppressWarnings("serial")
 public class AutenticacaoListener implements PhaseListener {
 
 	@Override
 	public void afterPhase(PhaseEvent event) {
-		System.out.println("DEPOIS DA FASE: " + event.getPhaseId());
+		String paginaAtual = Faces.getViewId();
+		
+		boolean ehPaginaDeAutenticacao = paginaAtual.contains("login.xhtml");
+	
+		if(!ehPaginaDeAutenticacao){
+			LoginBean autenticacaoBean = Faces.getSessionAttribute("loginBean");
+
+			if(autenticacaoBean == null){
+				Faces.navigate("/paginas/login.xhtml");
+				return;
+			}
+			
+			Usuario usuario = autenticacaoBean.getUsuarioLogado();
+			if(usuario == null){
+				Faces.navigate("/paginas/login.xhtml");
+				return;
+			}
+		}		
 	}
 
 	@Override
